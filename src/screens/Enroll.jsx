4447c4888301d8a94c444393e7cdb7635cf1f473
DevHola <56859027+DevHola@users.noreply.github.com>
2021-11-11 16:34:21 +0100

    trial mode
    trial mode

--- src/screens/Enroll.jsx
+++ src/screens/Enroll.jsx
@@ -49,7 +49,7 @@ export default function Enroll() {
 		<hr className="horizon"></hr>
 		<div className="Itemholder justify-content-between"><Carousel className="no ml-1" responsive={responsive}>
 		{Courses.map((c) => (
-			<div className="lex" >
+			<div className="lex" Key:{c._id} >
        	<div className="card tyer">
 		   <img className="card-img-top" src="https://images.unsplash.com/photo-1588702547919-26089e690ecc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8b25saW5lfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="Card cap"></img>
 		   <div className="card-body pro">
